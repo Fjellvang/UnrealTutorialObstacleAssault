64c136e5a152b4bf636efdc1a13e19f7bb12edd1
Aleksander <aleksander@fjellvang.dk>
2023-11-18 13:11:11 +0100

    initial

---
+++ Source/ObstacleAssault/MovingPlatform.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovingPlatform.h"
+
+// Sets default values
+AMovingPlatform::AMovingPlatform()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+	StartLocation = GetActorLocation();
+	MaxDistanceSquared = MaxDistance * MaxDistance;
+}
+
+// Called every frame
+void AMovingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	FVector CurrentLocation = GetActorLocation();
+
+	FVector Distance = CurrentLocation - StartLocation;
+
+	double DistanceSquared = Distance.SquaredLength();
+	SetActorLocation(CurrentLocation + (Speed * DeltaTime));
+	if ((MaxDistanceSquared) < DistanceSquared)
+	{
+		StartLocation = StartLocation + (MaxDistance * Speed.GetSafeNormal());
+		SetActorLocation(StartLocation);
+		Speed *= -1;
+	}
+
+}
+
